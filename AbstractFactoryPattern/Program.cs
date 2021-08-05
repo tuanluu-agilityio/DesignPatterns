9d3aa060aacf069a328f7a2eef1424207e961c7e
Tuan Luu <tuan.luu@dfinsolutions.com>
2021-08-05 20:11:54 +0700

    Add creational patterns

---
+++ AbstractFactoryPattern/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    /*
+     * An abstract factory is called a factory of factories. In this pattern, you provide a way to
+     * encapsulate a group of individual factories that have a common theme. In this process,
+     * you do not mention or specify their concrete classes.
+     * 
+     * This pattern helps you to interchange specific implementations without changing
+     * the code that uses them, even at runtime. However, it may result in unnecessary
+     * complexity and extra work. Even debugging becomes tough in some cases.
+     */
+
+    public interface IDog
+    {
+        void Speak();
+        void Action();
+    }
+
+    public interface ITiger
+    {
+        void Speak();
+        void Action();
+    }
+
+    #region Wild Animal collections
+    class WildDog : IDog
+    {
+        public void Action()
+        {
+            Console.WriteLine("Wild Dogs prefer to roam freely in jungles.\n"); 
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Wild Dog says: Bow-Wow.");
+        }
+    }
+
+    class WildTiger : ITiger
+    {
+        public void Action()
+        {
+            Console.WriteLine("Wild Tigers prefer hunting in jungles.\n");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Wild Tiger says: Halum.");
+        }
+    }
+    #endregion
+
+    #region Pet Animal collections
+    class PetDog : IDog
+    {
+        public void Action()
+        {
+            Console.WriteLine("Pet Dogs prefer to stay at home.\n");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Pet Dogs says: Bow-Wow.");
+        }
+    }
+
+    class PetTiger : ITiger
+    {
+        public void Action()
+        {
+            Console.WriteLine("Pet Tigers play in animal circus.\n");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Pet Tigers says: Halum.");
+        }
+    }
+    #endregion
+
+    // Abstract Factory
+    public interface IAnimalFactory
+    {
+        IDog GetDog();
+        ITiger GetTiger();
+    }
+
+    // Concrete Factory-Wild Animal Factory
+    public class WildAnimalFactrory : IAnimalFactory
+    {
+        public IDog GetDog()
+        {
+            return new WildDog();
+        }
+
+        public ITiger GetTiger()
+        {
+            return new WildTiger();
+        }
+    }
+
+    // Concrete Factory-Pet Animal Factory
+    public class PetAnimalFactory : IAnimalFactory
+    {
+        public IDog GetDog()
+        {
+            return new PetDog();
+        }
+
+        public ITiger GetTiger()
+        {
+            return new PetTiger();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Abstract Factory Pattern Demo***\n");
+
+            // Making a wild dog through WildAnimalFactory
+            IAnimalFactory wildAnimalFactory = new WildAnimalFactrory();
+            IDog wildDog = wildAnimalFactory.GetDog();
+            wildDog.Speak();
+            wildDog.Action();
+
+            // Making a wild tiger through WildAnimalFactory
+            ITiger wildTiger = wildAnimalFactory.GetTiger();
+            wildTiger.Speak();
+            wildTiger.Action();
+
+            Console.WriteLine("**********************");
+
+            // Making a pet dog through PetAnimalFactory
+            IAnimalFactory petAnimalFactory = new PetAnimalFactory();
+            IDog petDog = petAnimalFactory.GetDog();
+            petDog.Speak();
+            petDog.Action();
+
+            // Making a pet tiger through PetAnimalFactory
+            ITiger petTiger = petAnimalFactory.GetTiger();
+            petTiger.Speak();
+            petTiger.Action();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    /*
+     * Real-Life example:
+     *      In a restaurant, based on customer inputs, a chef varies the taste of dished to make the
+     *      final products.
+    */   
+
+    public interface IAnimal
+    {
+        void Speak();
+        void Action();
+
+        void Run();
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Action()
+        {
+            Console.WriteLine("Dogs prefer barking...\n");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Dogs running...");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Dogs says: Bow-Wow.");
+        }
+    }
+
+    public class Tiger : IAnimal
+    {
+        public void Action()
+        {
+            Console.WriteLine("Tigers prefer hungting...\n");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Tigers run too...");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Tiger says: Halum.");
+        }
+    }
+
+    public abstract class IAnimalFactory
+    {
+        public IAnimal MakeAnimal()
+        {
+            Console.WriteLine("\n IAnimalFactory.MakeAnimal() - You cannot ignore parent rules.");
+            IAnimal animal = CreateAnimal();
+            animal.Speak();
+            animal.Action();
+            animal.Run();
+
+            return animal;
+        }
+        public abstract IAnimal CreateAnimal();
+    }
+
+    public class DogFactory : IAnimalFactory
+    {
+        public override IAnimal CreateAnimal()
+        {
+            // creating a Dog
+            return new Dog();
+        }
+    }
+
+    public class TigerFactory : IAnimalFactory
+    {
+        public override IAnimal CreateAnimal()
+        {
+            // creating a Tiger
+            return new Tiger();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Factory Pattern Demo***\n");
+
+            // Creating a Tiger Factory
+            IAnimalFactory tigerFactory = new TigerFactory();
+            IAnimal aTiger = tigerFactory.MakeAnimal();
+
+            // Creating a tiger using the Factory method
+            //IAnimal aTiger = tigerFactory.CreateAnimal();
+            //aTiger.Speak();
+            //aTiger.Action();
+
+            // Creating a DogFactory
+            IAnimalFactory dogFactory = new DogFactory();
+            IAnimal aDog = dogFactory.MakeAnimal();
+
+            // Creating a dog using the Factory method
+            //IAnimal aDog = dogFactory.CreateAnimal();
+            //aDog.Speak();
+            //aDog.Action();
+
+            Console.ReadKey();
+        }
+    }
+}
