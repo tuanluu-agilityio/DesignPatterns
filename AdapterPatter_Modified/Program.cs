6e83adad26e47a496af924f09771da9a61f9d600
Tuan Luu <tuan.luu@dfinsolutions.com>
2021-08-06 22:28:03 +0700

    Add Adapter Pattern examples

---
+++ AdapterPatter_Modified/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace AdapterPatter_Modified
+{
+    interface RectInterface
+    {
+        void AboutRectangle();
+        double CalculateAreaOfRectangle();
+    }
+
+    class Rect : RectInterface
+    {
+
+        public double Length;
+
+        public double Width;
+
+        public Rect(double l, double w)
+        {
+            this.Length = l;
+            this.Width = w;
+        }
+
+
+        public void AboutRectangle()
+        {
+            Console.WriteLine("Actually, I am a Rectangle");
+        }
+
+        public double CalculateAreaOfRectangle()
+        {
+            return Length * Width;
+        }
+    }
+
+    interface TriInterface
+    {
+        void AboutTriangle();
+        double CalculateAreaOfTriangle();
+    }
+
+    class Triangle : TriInterface
+    {
+
+        public double BaseLength;
+        public double Height;
+        public Triangle(double b, double h)
+        {
+            this.BaseLength = b;
+            this.Height = h;
+        }
+
+        public void AboutTriangle()
+        {
+            Console.WriteLine("Actually, I am a Triangle");
+        }
+
+        public double CalculateAreaOfTriangle()
+        {
+            return 0.5 * BaseLength * Height;
+        }
+    }
+
+    class TriangoleAdapter : RectInterface
+    {
+        Triangle triangle;
+        public TriangoleAdapter(Triangle t)
+        {
+            this.triangle = t;
+        }
+
+        public void AboutRectangle()
+        {
+            triangle.AboutTriangle();
+        }
+
+        public double CalculateAreaOfRectangle()
+        {
+            return triangle.CalculateAreaOfTriangle();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Adapter Pattern Modified Demo***\n");
+
+            Rect r = new Rect(20, 10);
+            Console.WriteLine("Area of Rectangle is : {0} Square unit", r.CalculateAreaOfRectangle());
+
+            Triangle t = new Triangle(20, 10);
+            Console.WriteLine("Area of Triangle is: {0} Square unit", t.CalculateAreaOfTriangle());
+
+            RectInterface adapter = new TriangoleAdapter(t);
+            Console.WriteLine("Area of Triangle using the triangle adapter is: {0} Square unit", GetArea(adapter));
+
+            Console.ReadKey();
+        }
+
+        static double GetArea(RectInterface r)
+        {
+            r.AboutRectangle();
+            return r.CalculateAreaOfRectangle();
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    /*
+     * GoF Definition:
+     *  Convert the interface of a class into another interface that clients expect. The Adapter
+     *  pattern lets classes work together that could not otherwise because of incompatible interfaces.
+     */
+    class Rect
+    {
+
+        public double length;
+
+        public double width;
+    }
+
+    class Caculator
+    {
+
+        public double GetArea(Rect rect)
+        {
+            return rect.length * rect.width;
+        }
+    }
+
+    class Triangle
+    {
+
+        public double baseT;
+
+        public double height;
+        public Triangle(int b, int h)
+        {
+            this.baseT = b;
+            this.height = h;
+        }
+    }
+
+    class CalculatorAdapter
+    {
+
+        public double GetArea(Triangle triangle) 
+        {
+            Caculator c = new Caculator();
+            Rect rect = new Rect();
+            rect.length = triangle.baseT;
+            rect.width = triangle.height * 0.5;
+
+            return c.GetArea(rect);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Adapter Pattern demo***\n");
+            CalculatorAdapter cal = new CalculatorAdapter();
+            Triangle t = new Triangle(20, 10);
+            Console.WriteLine("Area of Triangle is: " + cal.GetArea(t) + " Square unit");
+            Console.ReadKey();
+        }
+    }
+}
