03589bab947d6c10798ae9f814b68a9953d8eb31
Tuan Luu <tuan.luu@dfinsolutions.com>
2021-08-15 22:51:36 +0700

    Add pattern examples

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace BridgePattern
+{
+    /*
+     * Concept:
+     *      This pattern is also known as the Handle/Body pattern. With it, you decouple an
+     *      implementation class from an abstract class by providing a bridge between them.
+     *      
+     *      This bridge interface makes the functionality of concrete classes independent from
+     *      the interface implementer classes. You can alter different kinds of classes structurally
+     *      without affecting each other.
+     */
+
+    // Implementor
+    public interface IState
+    {
+        void MoveState();
+    }
+
+    // ConcreteImplementor-1
+    public class OnState : IState
+    {
+        public void MoveState()
+        {
+            Console.Write("On State");
+        }
+    }
+
+    // ConcreteImplementor-2
+    public class OffState : IState
+    {
+        public void MoveState()
+        {
+            Console.Write("Off State");
+        }
+    }
+
+    // Abstraction
+    public abstract class ElectronicGoods
+    {
+        // Composition - implementor
+        protected IState state;
+
+        // Alternative approach to properties:
+        // we can also pass an implementor (as input argument) inside a
+        // constructor.
+        public IState State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+
+        abstract public void MoveToCurrentState();
+    }
+
+    // Refined Abstraction
+    public class Television : ElectronicGoods
+    {
+        public override void MoveToCurrentState()
+        {
+            Console.Write("\n Television is functioning at : ");
+            state.MoveState();
+        }
+    }
+
+    public class VCD : ElectronicGoods
+    {
+        public override void MoveToCurrentState()
+        {
+            Console.Write("\n VCD is functioning at : ");
+            state.MoveState();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Bridge Pattern Demo***");
+            Console.WriteLine("\nDealing with a Television");
+
+            ElectronicGoods eItem = new Television();
+            IState presentState = new OnState();
+            eItem.State = presentState;
+            eItem.MoveToCurrentState();
+
+            // Verifying Off state of the Television now
+            presentState = new OffState();
+            eItem.State = presentState;
+            eItem.MoveToCurrentState();
+
+            Console.WriteLine("\n\nDealing with a VCD:");
+            presentState = new OnState();
+            eItem = new VCD();
+            eItem.State = presentState;
+            eItem.MoveToCurrentState();
+
+            presentState = new OffState();
+            eItem.State = presentState;
+            eItem.MoveToCurrentState();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CompositePattern/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    interface IEmployee
+    {
+        void PrintStructures();
+    }
+
+    class CompositeEmployee : IEmployee
+    {
+        private string name;
+        private string dept;
+
+        // the container for child objects
+        private List<IEmployee> controls;
+
+        //constructor
+        public CompositeEmployee(string name, string dept)
+        {
+            this.name = name;
+            this.dept = dept;
+            controls = new List<IEmployee>();
+        }
+
+
+        public void Add(IEmployee e) 
+        {
+            controls.Add(e);
+        }
+
+
+        public void Remove(IEmployee e) 
+        {
+            controls.Remove(e);
+        }
+
+        public void PrintStructures()
+        {
+            Console.WriteLine("\t" + this.name + " works in " + this.dept);
+            foreach (IEmployee e in controls)
+            {
+                e.PrintStructures();
+            }
+        }
+    }
+
+    class Employee : IEmployee
+    {
+        private string name;
+        private string dept;
+
+        // constructor
+        public Employee(string name, string dept)
+        {
+            this.name = name;
+            this.dept = dept;
+        }
+
+        public void PrintStructures() 
+        {
+            Console.WriteLine("\t\t" + this.name + " works  in " + this.dept);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Composite Pattern Demo ***");
+            // Principal of the college
+            CompositeEmployee principal = new CompositeEmployee("Dr.S.Som(Principal)", "Planning-Supervising-Managing");
+
+            // The College has 2 head of departments-one from mathematics, one from computer sc.
+            CompositeEmployee hodMaths = new CompositeEmployee("Mrs.S.Das(HOD-Maths)", "Maths");
+            CompositeEmployee hodCompSc = new CompositeEmployee("Mr. V.Sarcar(HOD-CSE)", "Computer Sc.");
+
+            // 2 other teachers works in Mathematics department
+            Employee mathTeacher1 = new Employee("Math Teacher-1", "Maths");
+            Employee mathTeacher2 = new Employee("Math Teacher-2", "Maths");
+
+            // 3 other teachers works in Computer Sc. department
+            Employee cseTeacher1 = new Employee("CSE Teacher-1", "Computer Sc.");
+            Employee cseTeacher2 = new Employee("CSE Teacher-2", "Computer Sc.");
+            Employee cseTeacher3 = new Employee("CSE Teacher-3", "Computer Sc.");
+
+            // Teachers of Mathematics directly reports to HOD-Maths
+            hodMaths.Add(mathTeacher1);
+            hodMaths.Add(mathTeacher2);
+
+            // Teachers of Computer Sc. directly reports to HOD-CSE
+            hodCompSc.Add(cseTeacher1);
+            hodCompSc.Add(cseTeacher2);
+            hodCompSc.Add(cseTeacher3);
+
+            // Principal is on top of college
+            // HOD - Maths and Comp. Sc diretly reports to him
+            principal.Add(hodMaths);
+            principal.Add(hodCompSc);
+
+            Console.WriteLine("\n Testing the structure of a Principal object");
+            // Prints the complete structure
+            principal.PrintStructures();
+
+            Console.WriteLine("\n Testing the structure of a HOD object:");
+            Console.WriteLine("Teachers working at Computer Science department:");
+            // Prints the details of Computer Sc, department 
+            hodCompSc.PrintStructures();
+
+            // Leaf Node
+            Console.WriteLine("\n Testing the structure of a leaf node:");
+            mathTeacher1.PrintStructures();
+
+            // Suppose, one computer teacher is leaving now from the organization
+            hodCompSc.Remove(cseTeacher2);
+            Console.WriteLine("\n After CSE Teacher-2 resigned, the organization has following members:");
+            principal.PrintStructures();
+
+            Console.ReadKey();
+
+        }
+    }
+}
